399eafc3a0172c1b29ff7ce3d8e0f98543c93dd0
Jin Chengneng <jinchengneng@gmail.com>
2024-12-01 22:15:30 +0800

    commit

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import os
-from http.server import BaseHTTPRequestHandler
 
 app = Flask(__name__)
 app.template_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
@@ -15,21 +14,9 @@ def render_latex():
     text = text.replace('\n', '<br>')
     return jsonify({'text': text})
 
-class handler(BaseHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-    
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        response = app.handle_request()
-        self.wfile.write(response.get_data())
-    
-    def do_POST(self):
-        self.send_response(200)
-        self.end_headers()
-        response = app.handle_request()
-        self.wfile.write(response.get_data())
+# Vercel serverless handler
+app.debug = True
+handler = app.wsgi_app
 
 if __name__ == '__main__':
     app.run(debug=True) 

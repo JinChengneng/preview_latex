e90b066d97b799d51c956485ad485d986b48f6ab
Jin Chengneng <jinchengneng@gmail.com>
2024-12-01 22:13:01 +0800

    commit: fix api/index.py

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import os
+from http.server import BaseHTTPRequestHandler
 
 app = Flask(__name__)
 app.template_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
@@ -14,9 +15,21 @@ def render_latex():
     text = text.replace('\n', '<br>')
     return jsonify({'text': text})
 
-# Vercel serverless function handler
-def handler(environ, start_response):
-    return app.wsgi_app(environ, start_response)
+class handler(BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        response = app.handle_request()
+        self.wfile.write(response.get_data())
+    
+    def do_POST(self):
+        self.send_response(200)
+        self.end_headers()
+        response = app.handle_request()
+        self.wfile.write(response.get_data())
 
 if __name__ == '__main__':
     app.run(debug=True) 
